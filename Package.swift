72eaed36c3f37177d04db5e37cddc520317e6f43
Zoltán Magyar <zoltanmagyar28@icloud.com>
2026-02-10 13:02:21 +0100

    initial commit

---
+++ Package.swift
@@ -0,0 +1,31 @@
+// swift-tools-version: 6.2.3
+import PackageDescription
+
+let package = Package(
+    name: "OpenConnectKit",
+    platforms: [.macOS(.v26)],
+    products: [
+        .library(name: "OpenConnectKit", targets: ["OpenConnectKit"])
+    ],
+    targets: [
+        .systemLibrary(
+            name: "COpenConnectLib",
+            pkgConfig: "openconnect",
+            providers: [
+                .brew(["openconnect"]),
+                .apt(["libopenconnect-dev"]),
+            ]
+        ),
+        .target(
+            name: "COpenConnect",
+            dependencies: ["COpenConnectLib"]
+        ),
+        .target(
+            name: "OpenConnectKit",
+            dependencies: ["COpenConnect"],
+            swiftSettings: [
+                .enableUpcomingFeature("NonisolatedNonsendingByDefault")
+            ]
+        ),
+    ]
+)
